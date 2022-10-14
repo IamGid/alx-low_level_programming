b835d527047c57af291f60f58708c6e26e6a5b6d
IamGid <johngideon23@gmail.com>
2022-10-14 08:07:07 -0700

    low level

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Prints a text
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
