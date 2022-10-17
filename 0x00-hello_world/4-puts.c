4a6241dc4963268ee667517c3d695232c20f79cb
IamGid <johngideon23@gmail.com>
2022-10-17 07:55:34 -0700

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry
- * Return: Always 0
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\"Programminig is like building a multilingual puzzle");
+	puts("\ "Programming is like building a multilingual puzzle");
 	return (0);
 }
